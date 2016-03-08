3058ac81af364fe1e94bcb558b18a5060370076c
Sten Henström <stenhen@hotmail.com>
2016-03-08 17:05:50 +0100

    17.05

--- src/kontorsprylar/ViewModels/PartialDescriptionViewModel.cs
+++ src/kontorsprylar/ViewModels/PartialDescriptionViewModel.cs
@@ -7,5 +7,11 @@ namespace kontorsprylar.ViewModels
 {
     public class PartialDescriptionViewModel
     {
+        public string ProductName = "TV-apparat (exkl. antennen på bild)";// { get; set; }
+        public float Price = 100.00F;  // { get; set; }
+        public float CampaignPrice { get; set; }
+        public string ImgLink = "tv.jpg"; // { get; set; }
+        public string Description = "Liten och praktisk tv. Svartvit bild. Stöd för två kanaler."; // { get; set; }
+        public int StockQuantity = 56; // { get; set; }
     }
 }
