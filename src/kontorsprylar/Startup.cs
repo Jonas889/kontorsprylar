3f37237868d99bcc0f763e26f29252e8009bf21f
Senad Pasalic <senadpasalic@live.com>
2016-03-08 15:41:29 +0100

    15:21 context

--- src/kontorsprylar/Startup.cs
+++ src/kontorsprylar/Startup.cs
@@ -24,6 +24,8 @@ namespace kontorsprylar
                 options.UseSqlServer(connString));
 
             services.AddMvc();
+
+            //testestest
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
