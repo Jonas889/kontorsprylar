1160b43dd6af1db2ce9f175c61b78895b4f24cb7
jonas889 <jonas@meljoner.se>
2016-03-08 15:58:25 +0100

    15:58 model done

---
+++ src/kontorsprylar/Models/ProductsInOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kontorsprylar.Models
+{
+    public class ProductsInOrder
+    {
+        public int OrderID { get; set; }
+    }
+}
