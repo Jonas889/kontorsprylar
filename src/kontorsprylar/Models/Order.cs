223725333105fcf53fa940de4ff73145b688e173
jonas889 <jonas@meljoner.se>
2016-03-08 15:27:09 +0100

    15:26 Ny model

---
+++ src/kontorsprylar/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kontorsprylar.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string DeliveryService { get; set; }
+        public string DeliveryAdress { get; set; }
+    }
+}
