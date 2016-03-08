331f6f9adcc12ff5524a484a8f562f3aeebe543e
Sten Henström <stenhen@hotmail.com>
2016-03-08 15:59:11 +0100

    15.59 Models klara

---
+++ src/kontorsprylar/Models/Specification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kontorsprylar.Models
+{
+    public class Specification
+    {
+        public int CategoryID { get; set; }
+        public string SpecKey { get; set; }
+        public string SpecValue { get; set; }
+    }
+}
