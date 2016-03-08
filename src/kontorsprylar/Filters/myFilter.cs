152ef8b3378f0258e4daaa8b90e6da2d46be4409
jonas889 <jonas@meljoner.se>
2016-03-08 11:58:05 +0100

    first commit!

---
+++ src/kontorsprylar/Filters/myFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Övning3.Filters
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class MustBeTrueAttribute : ValidationAttribute, IClientModelValidator
+    {
+       
+
+        public override bool IsValid(object value)
+        {
+            return value != null && value is bool && (bool)value;
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ClientModelValidationContext context)
+        {
+            yield return new ModelClientValidationRule("mustbetrue", ErrorMessage = ErrorMessageString);
+        }
+    }
+}
+
---
+++ src/kontorsprylar/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.Entity;
+
+namespace kontorsprylar
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connString = @"";
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<lägg till ett context>(options =>
+                options.UseSqlServer(connString));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
