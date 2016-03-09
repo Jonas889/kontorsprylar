6b41b01e16de4c42567031b1d12c4e2ebcf065db
herrsten <sannastenlund@hotmail.com>
2016-03-09 09:48:48 +0100

    09.48

--- src/kontorsprylar/ViewModels/RegistrateViewModel.cs
+++ src/kontorsprylar/ViewModels/RegistrateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,31 @@ namespace kontorsprylar.ViewModels
 {
     public class RegistrateViewModel
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Förnamn")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Efternamn")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Gatuadress")]
+        public string Street { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Postkod")]
+        public string Zip { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Postort")]
+        public string City { get; set; }
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Land")]
+        //public string Country { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Mobilnummer")]
+        public string CellPhone { get; set; }
+        [Display(Name = "Hemtelefonnummer (valfritt)")]
+        public string HomePhone { get; set; }
+
+        [Required(ErrorMessage = "Obligatorisk"), EmailAddress(ErrorMessage = "Ange en korrekt e-postaddress"), Display(Name = "E-post")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Obligatoriskt"), Display(Name = "Lösenord")] //<-- fixas
+        public string Password { get; set; }
+
+
     }
 }
