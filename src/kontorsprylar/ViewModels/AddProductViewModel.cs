b2783b63e9bb19001b3bb7424d0a62c50372d10c
Senad Pasalic <senadpasalic@live.com>
2016-03-09 13:20:38 +0100

    13:20

---
+++ src/kontorsprylar/ViewModels/AddProductViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kontorsprylar.ViewModels
+{
+    public class AddProductViewModel
+    {
+        [Display(Name = "Produkt namn")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fyll i produktnamn")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Produkt beskrivning")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fyll i beskrivning")]
+        public string Description { get; set; }
+
+        [Display(Name = "Pris")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fyll i pris")]
+        public float Price { get; set; }
+
+        [Display(Name = "Kampanj pris")]
+        public float CampaignPrice { get; set; }
+
+        [Display(Name = "Antal")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fyll i antal")]
+        public int StockQuantity { get; set; }
+
+        [Display(Name = "Bild")]        
+        public string Picture { get; set; }
+
+        [Display(Name = "Rea")]
+        public bool ForSale { get; set; }
+    }
+}
