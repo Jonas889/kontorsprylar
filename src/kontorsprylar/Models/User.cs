2b3bb648e378e685d1a6cbff64fda2467b0318dd
Sten Henström <stenhen@hotmail.com>
2016-03-09 10:02:28 +0100

    10.02

--- src/kontorsprylar/Models/User.cs
+++ src/kontorsprylar/Models/User.cs
@@ -12,6 +12,10 @@ namespace kontorsprylar.Models
         public string Address { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CellPhone { get; set; }
+        public string Phone { get; set; }
+        public  string Password { get; set; }
         public string CompanyName { get; set; }
     }
 }
