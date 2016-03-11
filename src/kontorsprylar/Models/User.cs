cbf7076b27577086fd154b972873cce611afb2f0
jonas889 <jonas@meljoner.se>
2016-03-11 11:19:33 +0100

    11:19

--- src/kontorsprylar/Models/User.cs
+++ src/kontorsprylar/Models/User.cs
@@ -8,7 +8,7 @@ namespace kontorsprylar.Models
     public class User
     {
         public int UserID { get; set; }
-        public int Accessability { get; set; }
+        public string Accessability { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
