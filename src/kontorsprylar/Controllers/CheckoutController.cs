dd22f0c2216fd4da4fd8fe15863b65744ef5d076
jonas889 <jonas@meljoner.se>
2016-03-12 16:56:23 +0100

    16:56

--- src/kontorsprylar/Controllers/CheckoutController.cs
+++ src/kontorsprylar/Controllers/CheckoutController.cs
@@ -23,16 +23,16 @@ namespace kontorsprylar.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-
-
-            var shoppingList = dataManager.GetMyShoppingCart();
+            var shoppingList = new ShopingCart(); ;
+            if (HttpContext.Session.GetObjectFromJson<ShopingCart>("Cart") != null)
+                shoppingList = HttpContext.Session.GetObjectFromJson<ShopingCart>("Cart");
                 //.Select(p => new ShoppingCartVM
                 //{
                 //    ProductName = p.ProductName,
                 //    Price = p.Price,
                 //    ProductQuantity = p.ProductQuantity,
                 //    ProductID = p.ProductID
-                    
+
                 //}).ToList();
 
             return View(shoppingList);
@@ -42,8 +42,10 @@ namespace kontorsprylar.Controllers
 
         public IActionResult Delete(int productID)
         {
-           
-            var result = dataManager.DeleteFromCart(productID);
+            var shoppingList = new ShopingCart(); ;
+            if (HttpContext.Session.GetObjectFromJson<ShopingCart>("Cart") != null)
+                shoppingList = HttpContext.Session.GetObjectFromJson<ShopingCart>("Cart");
+            var result = dataManager.DeleteFromCart(shoppingList, productID);
             return RedirectToAction("index", result);
         }
 
--- src/kontorsprylar/Controllers/ShoppingCartController.cs
+++ src/kontorsprylar/Controllers/ShoppingCartController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using kontorsprylar.Models;
+using kontorsprylar.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,19 +12,21 @@ namespace kontorsprylar.Controllers
 {
     public class ShoppingCartController : Controller
     {
-
-       
-            static StoredDbContext context;
-            public static DataManager dataManager;
-            public ShoppingCartController(StoredDbContext newcontext)
-            {
-                context = newcontext;
-                dataManager = new DataManager(context);
-            }
-            // GET: /<controller>/
-            public IActionResult AddToCart(int PID)
+        static StoredDbContext context;
+        public static DataManager dataManager;
+        public ShoppingCartController(StoredDbContext newcontext)
         {
-            var model = dataManager.GetMyShoppingCart(PID);
+            context = newcontext;
+            dataManager = new DataManager(context);
+        }
+        // GET: /<controller>/
+        public IActionResult AddToCart(int PID)
+        {
+            var kundVagn = new ShopingCart();
+            if (HttpContext.Session.GetObjectFromJson<ShopingCart>("Cart") != null)
+                kundVagn = HttpContext.Session.GetObjectFromJson<ShopingCart>("Cart");
+            var model = dataManager.GetMyShoppingCart(kundVagn, PID);
+            HttpContext.Session.SetObjectAsJson("Cart", kundVagn);
             return PartialView("ShoppingCartPartial", model);
         }
     }
